c64e0f0a7ef2526e43ca766317fe654fd9c8d757
RaptorTech4 <33655338+RaptorTech4@users.noreply.github.com>
2018-04-21 12:33:08 +0200

    Zombie

--- Assets/Script/Zombie.cs
+++ Assets/Script/Zombie.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Zombie : MonoBehaviour {
 
     public int Value;
 
     public float speed;
-    
+    public UnityEvent IDie;
  	
 	void Update ()
     {
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, Player._Player.transform.position, step);
 	}
+
+    public void ZombieDie()
+    {
+        IDie.Invoke();
+    }
+
 }
