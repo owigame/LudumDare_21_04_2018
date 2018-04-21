4ab420c037450c2b1a12ab7f9e434a099dbd4f37
Andre Hamman <hammanandre@gmail.com>
2018-04-21 18:16:53 +0200

    Added Puzzle Script

---
+++ Assets/Script/PuzzleTotal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTotal : MonoBehaviour {
+
+	public int total;
+	void Start () {
+		Scoring score = FindObjectOfType<Scoring> ();
+		score.RequiredReached.AddListener (IncTotal);
+	}
+	void IncTotal () {
+		total++;
+	}
+
+}
