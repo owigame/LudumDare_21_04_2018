532deed2bc20e583610d2a7560849d8dc7c652d5
RaptorTech4 <33655338+RaptorTech4@users.noreply.github.com>
2018-04-21 13:18:30 +0200

    Zombie I die, spawn tracking

--- Assets/Script/Zombie.cs
+++ Assets/Script/Zombie.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Zombie : MonoBehaviour {
 
     public int Value;
 
     public float speed;
-    
+    public UnityEvent IDie;
  	
 	void Update ()
     {
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, Player._player.transform.position, step); 
 	}
+
+    public void RombieDie()
+    {
+
+
+        Destroy(gameObject);
+    }
+
 }
