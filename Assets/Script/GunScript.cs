a3d4aedeffa87b21d0ebb1a04bc356993913cf62
Vandermerwer <38580042+Vandermerwer@users.noreply.github.com>
2018-04-21 11:28:38 +0200

    total score base structure

---
+++ Assets/Script/GunScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class GunScript : MonoBehaviour
+{
+    public UnityEvent<Operator, int> HitEvent;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+public enum Operator
+{
+plus,
+minus
+}
---
+++ Assets/Script/Scoring.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour {
+
+    public float TotalScore;
+
+	void Start () {
+
+        GunScript[] AllGuns = FindObjectsOfType<GunScript>();
+        foreach (var gun in AllGuns)
+        {
+            gun.HitEvent.AddListener(UpdateScore);
+        }
+	}
+
+    void UpdateScore(Operator OP, int VAL)
+    {
+        
+    }
+}
