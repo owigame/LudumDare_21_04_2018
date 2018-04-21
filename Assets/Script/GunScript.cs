6cbf5a31612405ee3ca1d85b7e2851e57a359029
Jared Brandjes <4t0m1c.w07f@gmail.com>
2018-04-21 13:02:11 +0200

    That one annoying steamVR fix
    You made the right choice!

--- Assets/Script/GunScript.cs
+++ Assets/Script/GunScript.cs
@@ -1,28 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Events;
-using System.Collections;
 
-public class GunScript : MonoBehaviour
-{
+public class GunScript : MonoBehaviour {
     public UnityEvent<Operator, int> HitEvent;
     public Operator opp;
     //int raycastLayerMask = ;
 
     RaycastHit hit;
 
-    private void Shoot()
-    {
-        if (Physics.Raycast(transform.position, transform.forward, Mathf.Infinity))
-        {
-            if (hit.transform.tag == "enemy")
-            {
-                HitEvent.Invoke(opp, GetComponent<Zombie>().Value);
+    private void Shoot () {
+        if (Physics.Raycast (transform.position, transform.forward, out hit, Mathf.Infinity)) {
+            if (hit.transform.tag == "enemy") {
+                HitEvent.Invoke (opp, GetComponent<Zombie> ().Value);
             }
         }
     }
 }
-public enum Operator
-{
+public enum Operator {
     plus,
     minus,
     multiply,
