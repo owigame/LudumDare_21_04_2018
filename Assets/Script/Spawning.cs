5b950ae6b01075e8eb495b1334c1bcc4a987b657
RaptorTech4 <33655338+RaptorTech4@users.noreply.github.com>
2018-04-21 11:00:10 +0200

    Update

---
+++ Assets/Script/Spawning.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    //Bennet
+
+
+    public GameObject SpawnPosition;
+    public GameObject SpawnOBJ;
+
+
+    void SpawnEnemy()
+    {
+
+    }
+	
+}
