198fab03f32a757fb58fc2c31e59d25ceb1de297
Andre Hamman <hammanandre@gmail.com>
2018-04-21 12:56:32 +0200

    Score listen and update

---
+++ Assets/Script/PuzzleTotal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTotal : MonoBehaviour {
+
+	int total;
+	void Start () {
+		Scoring score = FindObjectOfType<Scoring> ();
+		score.RequiredReached.AddListener (TotalPuzzle);
+	}
+	void TotalPuzzle () {
+		total++;
+	}
+}
