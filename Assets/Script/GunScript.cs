01fd60deaecae1f30187503447a2c434aed3467b
Jared Brandjes <jared@openwindow.co.za>
2018-04-22 18:28:50 +0200

    Gun Rotary in prog

--- Assets/Script/GunScript.cs
+++ Assets/Script/GunScript.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class GunScript : MonoBehaviour {
 
     public delegate void OppEvents(Operator _opp);
-    public static OppEvents OnOppChanged;
+    public OppEvents OnOppChanged;
 
     public static GunScript _GunScript;
 
@@ -33,6 +33,9 @@ public class GunScript : MonoBehaviour {
 
     RaycastHit hit;
     GameObject lastHit;
+
+    [Header("Rotary")]
+    public CircularDriveModded _CircularDriveModded;
 
     private void Shoot () {
         if (Physics.Raycast (transform.position, pointer.forward, out hit, Mathf.Infinity, _mask)) {
@@ -72,12 +75,16 @@ public class GunScript : MonoBehaviour {
         _VRTK_ControllerEvents.TouchpadAxisChanged += new ControllerInteractionEventHandler (OperatorChange);
         _VRTK_ControllerEvents.TriggerClicked += new ControllerInteractionEventHandler (Shoot);
         _VRTK_ControllerEvents.TriggerAxisChanged += new ControllerInteractionEventHandler (GunTrigger);
+        _VRTK_ControllerEvents.GripPressed += new ControllerInteractionEventHandler (GripPressed);
+        _VRTK_ControllerEvents.GripReleased += new ControllerInteractionEventHandler (GripReleased);
     }
 
     private void OnDisable () {
         _VRTK_ControllerEvents.TouchpadAxisChanged -= new ControllerInteractionEventHandler(OperatorChange);
         _VRTK_ControllerEvents.TriggerClicked -= new ControllerInteractionEventHandler(Shoot);
         _VRTK_ControllerEvents.TriggerAxisChanged -= new ControllerInteractionEventHandler (GunTrigger);
+        _VRTK_ControllerEvents.GripPressed -= new ControllerInteractionEventHandler (GripPressed);
+        _VRTK_ControllerEvents.GripReleased -= new ControllerInteractionEventHandler (GripReleased);
     }
 
     void Update () {
@@ -179,7 +186,19 @@ public class GunScript : MonoBehaviour {
 
     void GunTrigger(object sender, ControllerInteractionEventArgs _args)
     {
-        _anim.Play("Trigger", 1, _args.buttonPressure);
+        Debug.Log("Trigger: " + _args.buttonPressure);
+        _anim.Play("Trigger", -1, _args.buttonPressure);
     }
 
+    void GripPressed(object sender, ControllerInteractionEventArgs _args)
+    {
+        //Get position of controller relative to watch center
+        //Rotate around watch center
+
+    }
+
+    void GripReleased(object sender, ControllerInteractionEventArgs _args)
+    {
+        //Submit multiplier
+    }
 }
