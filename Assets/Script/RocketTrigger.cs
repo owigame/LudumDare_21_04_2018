ffb23bcfe36236e66e929f0201da2bad6d94fad5
Jared Brandjes <jared@openwindow.co.za>
2018-04-23 22:49:17 +0200

    Big Stuff
    Enemy Colliders
    Rockets explosions

---
+++ Assets/Script/RocketTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketTrigger : MonoBehaviour {
+
+	public Rocket _rocket;
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+	}
+
+	private void OnTriggerEnter (Collider other) {
+		_rocket.OnExplode (other.transform.gameObject.layer == 8, (other.transform.gameObject.layer == 8 ? other.attachedRigidbody.gameObject : other.gameObject));
+	}
+}
